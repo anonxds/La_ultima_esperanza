77043303161b9dd442476c9b1e1c8f48c40ca88b
orlandoxds <34320335+orlandoxds@users.noreply.github.com>
2019-12-16 09:36:46 -0800

    Initial commit

---
+++ app/src/main/java/com/example/la_ultima_esperanza/setting.java
@@ -0,0 +1,19 @@
+package com.example.la_ultima_esperanza;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class setting extends BottomSheetDialogFragment {
+
+}
